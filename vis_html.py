559e42796ef29c7bf86ffce17a586f9653053a2e
root <root>
2021-05-24 15:43:12 -0500

    add face recognition

--- vis_html.py
+++ vis_html.py
@@ -3,9 +3,9 @@ from klampt import WorldModel, vis
 world = WorldModel()
 world.readFile("world.xml")
 
-vis.init("HTML")
+vis.init("IPython")
 vis.add("world", world)
 vis.show()
-string = vis.nativeWindow().page()
-with open("vis.html", "w") as f:
-    f.write(string)
+#string = vis.nativeWindow().page()
+#with open("vis.html", "w") as f:
+#    f.write(string)
